e49ca768d2ec573c72eb8f13780aa08fa33f665a
Jurylord <68111227+Jurylord@users.noreply.github.com>
2020-07-10 17:30:42 +0530

    Add files via upload

---
+++ trial.py
@@ -0,0 +1,11 @@
+
+
+chck=str(input("Enter the filename :"))
+if (".py" in chck):
+    print("extension of filename is: PYTHON")
+elif (".cpp" in chck):
+    print("extension of filename is: C++")
+elif (".c" in chck):
+    print("extension of filename is: C")
+elif (".class" in chck):
+    print("extension of filename is: JAVA")
